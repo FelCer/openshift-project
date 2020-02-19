0144d1f8af2722970518e70b036f8a5419138c57
Luis Felipe Cerero Garcia <luisfelipe.cerero@kycus.io>
2020-02-19 08:59:55 -0500

    test, delete process.env.OPENSHIFT_NODEJS_IP

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app     = express();
 
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT ||  8080;
+var ip   = process.env.IP   || '0.0.0.0';
 var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
 
 app.set('views', __dirname + '/views');
